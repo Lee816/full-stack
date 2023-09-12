6d55a6d26fbce761e63283220b85048ac565f8df
Lee816 <lgha0816@naver.com>
2023-09-12 09:47:41 +0900

    유저 등록 serializer, viewsets, router 등록

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 
 from .user.viewsets import UserViewSet
+from .auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 
@@ -9,6 +10,7 @@ router = routers.SimpleRouter()
 # 뷰셋 - 유효한 뷰셋 클래스만을 나타낸다.
 # basename - 선택적 인자이지만 가독성을 높여주기 때문에 사용하는 것을 권장
 router.register(r'user',UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 urlpatterns = [
     *router.urls,
 ]
