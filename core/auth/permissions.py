17a0dd37efce5a88024e8468daff198e51a92b4d
Lee816 <lgha0816@naver.com>
2023-09-14 10:35:35 +0900

    UserPermission 잘못 작성한것 수정

---
+++ core/auth/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
+
+# Django 구너한은 보통 두 단계에서 작동 - 전체엔드포인드(has_permisson) 와 객체 수준(has_object_permission)
+# 권한을 작성하는 좋은 방법은 항상 기본적으로 거부하는것
+# 익명 사용자는 SAFE_METHODS (GET, OPTIONS, HEAD) 요청만 할 수 있게 했다.
+class UserPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_anonymous:
+            return request.method in SAFE_METHODS
+        
+        if view.basename in ['post']:
+            return bool(request.user and request.user.is_authenticated)
+
+        return False
+    
+    def has_permission(self, request, view):
+        if view.basename in ['post']:
+            if request.user.is_anonymous:
+                return request.method in SAFE_METHODS
+            
+            return bool(request.user and request.user.is_authenticated)
+        
+        return False
--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from core.abstract.viewsets import AbstractViewSet
-from core.post.viewsets import UserPermission
+from core.auth.permissions import UserPermission
 
 from .models import Comment
 from .serializers import CommentSerializer
