dc55477ae6034b756910c1ba7d8d703eb3e177ce
Lee816 <lgha0816@naver.com>
2023-09-14 13:38:44 +0900

    오타 수정

--- core/user/tests.py
+++ core/user/tests.py
@@ -32,9 +32,9 @@ def test_create_user():
 @pytest.mark.django_db
 def test_create_superuser():
     user = User.objects.create_superuser(**data_superuser)
-    assert user.username == data_user['username']
-    assert user.email == data_user['email']
-    assert user.first_name == data_user['first_name']
-    assert user.last_name == data_user['last_name']
+    assert user.username == data_superuser['username']
+    assert user.email == data_superuser['email']
+    assert user.first_name == data_superuser['first_name']
+    assert user.last_name == data_superuser['last_name']
     assert user.is_superuser ==True
     assert user.is_staff == True
