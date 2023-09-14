cc13e62d5e07ab2f50bccbe645b01e538b670398
Lee816 <lgha0816@naver.com>
2023-09-14 11:22:34 +0900

    comment 뷰셋 강사님 코드로 변경

--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -16,17 +16,21 @@ class CommentViewSet(AbstractViewSet):
     # 중첩 라우터 사용으로 메소드 재정의
     
     def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs['post_pk'])
+        if self.request.user.is_superuser:
+            return Comment.objects.all()
+        post_pk = self.kwargs['post_pk']
+        if post_pk is None:
+            return Http404
+        queryset = Comment.objects.filter(post__public_id=post_pk)
+        return queryset
 
     def get_object(self):
-        try:
-            return Comment.objects.get(post_id=self.kwargs['post_pk'], id=self.kwargs['pk'])
-        except Comment.DoesNotExist:
-            raise Http404
-        
+        obj = Comment.objects.get_object_by_public_id(self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+        return obj
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-
         return Response(serializer.data, status=status.HTTP_201_CREATED)
