016f1a4bfd453c1ecb409524f3bfd95f689053ae
Lee816 <lgha0816@naver.com>
2023-09-18 14:15:06 +0900

    RegistrationForm 작성

--- social-media-app/src/components/authentication/RegistrationForm.jsx
+++ social-media-app/src/components/authentication/RegistrationForm.jsx
@@ -11,5 +11,131 @@ function RegistrationForm() {
     const [form, setForm] = useState({});
     const [error, setError] = useState(null);
 
-    return (<></>)
-};
+    // 폼 제출 처리 함수
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const registrationForm = event.currentTarget;
+        if (registrationForm.checkValidity() === false) {
+            event.stopPropagation();
+        }
+        setValidated(true);
+        const data = {
+            username: form.username,
+            password: form.password,
+            email: form.email,
+            first_name: form.first_name,
+            last_name: form.last_name,
+            bio: form.bio
+        };
+
+        // axios를 사용하여 API에 POST요청
+        axios
+            .post("http://localhost:8000/api/auth/register/", data)
+            .then((res) => {
+                localStorage.setItem(
+                    'auth', JSON.stringify({
+                        access: res.data.access,
+                        refresh: res.data.refresh,
+                        user: res.data.user,
+                    })
+                )
+                navigate('/')
+            })
+            .catch((err) => {
+                if (err.message) {
+                    setError(err.request.response)
+                }
+            })
+    }
+
+    return (
+        <Form
+            id='registration-form'
+            className="border p-4 rounded"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+        >
+            <Form.Group className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                    value={form.first_name}
+                    onChange={(e) => setForm({ form, first_name: e.target.value })}
+                    required
+                    type="text"
+                    placeholder="Enter first name"
+                />
+                <Form.Control.Feedback type="invalid">
+                    This field is required.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                    value={form.last_name}
+                    onChange={(e) => setForm({ form, last_name: e.target.value })}
+                    required
+                    type="text"
+                    placeholder="Enter last name"
+                />
+                <Form.Control.Feedback type="invalid">
+                    This field is required.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    value={form.username}
+                    onChange={(e) => setForm({ form, username: e.target.value })}
+                    required
+                    type="text"
+                    placeholder="Enter username"
+                />
+                <Form.Control.Feedback type="invalid">
+                    This field is required.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                    value={form.email}
+                    onChange={(e) => setForm({ form, email: e.target.value })}
+                    required
+                    type="email"
+                    placeholder="Enter email"
+                />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid email.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    value={form.password}
+                    onChange={(e) => setForm({ form, password: e.target.value })}
+                    required
+                    type="password"
+                    placeholder="Password"
+                />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid password.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Bio</Form.Label>
+                <Form.Control
+                    value={form.bio}
+                    onChange={(e) => setForm({ form, WebTransportBidirectionalStream: e.target.value })}
+                    type="textarea"
+                    placeholder="A simple bio ... (Optional)"
+                />
+            </Form.Group>
+            <div className="text-content text-danger">{error && <p>{error}</p>}</div>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default RegistrationForm
