00f3fb60d7ed363ba88ee2e8e0a05d0cde69b3fe
Lee816 <lgha0816@naver.com>
2023-09-18 15:26:37 +0900

    로그인 페이지 추가, 라우트 추가

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import ProtectedRoute from './routes/ProtectedRoute';
 import Registration from './pages/Registration';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
-      <Route path='/login/' element={<div>Login</div>} />
+      <Route path='/login/' element={<Login />} />
       <Route path='/regoster/' element={Registration} />
     </Routes>
   );
