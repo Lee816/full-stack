03f89d6247a8d0c4f9c4d11110c24056ad092485
Lee816 <lgha0816@naver.com>
2023-09-19 14:32:38 +0900

    에러 수정

--- social-media-app/src/helpers/axios.js
+++ social-media-app/src/helpers/axios.js
@@ -1,48 +1,52 @@
-import axios from "axios";
-import createAuthRefreshInterceptor from "axios-auth-refresh";
-import { getAccessToken, getRefreshToken, getUser } from '../hooks/user.actions'
+import axios from "axios"
+import createAuthRefreshInterceptor from "axios-auth-refresh"
+import { getAccessToken, getRefreshToken, getUser } from "../hooks/user.actions"
 
 const axiosService = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://localhost:8000/api",
     headers: {
         "Content-Type": "application/json",
     },
-});
+})
 
 // 1. 요청에 헤더를 추가하기 위해 요청 인터셉터를 작성
 axiosService.interceptors.request.use(async (config) => {
-    // 로컬 스토리지에서 액세스 토큰을 가져와 요청의 헤더에 추가
-    const { access } = JSON.parse(localStorage.getItem("auth"));
-    config.headers.Authorization = `Bearer ${access}`;
     config.headers.Authorization = `Bearer ${getAccessToken()}`
-    return config;
-});
+    return config
+})
+
 
 // 2. 요청 처리하고 성공 또는 실패시 해결된 또는 거부된 프로미스를 반환하는 응답 인터셉터를 작성
 axiosService.interceptors.response.use(
     (res) => Promise.resolve(res),
-    (err) => Promise.reject(err),
+    (err) => Promise.reject(err)
 )
 
 // 3. 요청이 실패한 경우 리스레시 토큰을 갱신하는 로직이 포함된 함수를 작성 - 실패한 요청이 401 오류를 반환할 때마다 호출
 const refreshAuthLogic = async (failedRequest) => {
-    // 로컬 스토리지 에서 리프레시 토큰을 가져와 새 액세스 토큰을 요청
-    const { refresh } = JSON.parse(localStorage.getItem('auth'));
     return axios
-        .post('/auth/refresh/', {
+        .post(
+            "/auth/refresh/",
+            {
                 refresh: getRefreshToken(),
-            }, {
-                baseURL: 'http://localhost:8000/api',
-        })
+            },
+            {
+                baseURL: "http://localhost:8000/api",
+            }
+        )
         .then((resp) => {
-            const { access, refresh } = resp.data;
-            failedRequest.response.config.headers['Authorization'] = 'Bearer ' + access;
-            localStorage.setItem('auth', JSON.stringify({ access, refresh }));
+            const { access } = resp.data
+            failedRequest.response.config.headers["Authorization"] =
+                "Bearer " + access
+            localStorage.setItem(
+                "auth",
+                JSON.stringify({ access, refresh: getRefreshToken(), user: getUser() })
+            )
         })
         .catch(() => {
-            localStorage.removeItem('auth');
-        });
-};
+            localStorage.removeItem("auth")
+        })
+}
 
 // 4. 인증 인터셉터를 초기화하고 사용자 정의 fetcher를 생성
 createAuthRefreshInterceptor(axiosService, refreshAuthLogic);
--- social-media-app/src/pages/Registration.jsx
+++ social-media-app/src/pages/Registration.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Link } from 'react-bootstrap';
-import RegistrationForm from '../components/authentication/RegistrationForm';
+import React from "react"
+
+import { Link } from "react-router-dom"
+
+import RegistrationForm from "../components/authentication/RegistrationForm"
 
 function Registration() {
     return (
@@ -10,10 +12,11 @@ function Registration() {
                     <div className="content text-center px-4">
                         <h1 className="text-primary">Welcome to Postagram!</h1>
                         <p className="content">
-                            This is a new social media site that will allow you to share your thoughts and experiences with your friends. Register now and start enjoying!
-                            <br />
+                            This is a new social media site that will allow you to share your
+                            thoughts and experiences with your friends. Register now and start
+                            enjoying! <br />
                             Or if you already have an account, please{" "}
-                            <Link to='/login/'>login</Link>
+                            <Link to="/login/">login</Link>
                         </p>
                     </div>
                 </div>
