727188701f693b12a887cfc8febc8b90ee707c18
Lee816 <lgha0816@naver.com>
2023-09-19 09:20:54 +0900

    custom user action hook 추가

---
+++ social-media-app/src/hooks/user.actions.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { userNavigate } from 'react-router-dom';
+
+function useUserActions() {
+    const navigate = userNavigate()
+    const baseURL = 'http://localhost:8000/api'
+    return {
+        login,
+        // register,
+        logout,
+    }
+
+    function setUserDate(data) {
+        localStorage.setItem('auth', JSON.stringify({
+            access: data.access,
+            refresh: data.refresh,
+            user: data.user,
+        }))
+    }
+
+    function login(data) {
+        return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+            setUserDate(data)
+            navigate('/')
+        })
+    }
+
+    function logout() {
+        localStorage.removeItem('auth')
+        navigate('/login')
+    }
+}
+
+export default useUserActions
